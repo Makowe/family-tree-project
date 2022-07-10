689eebb5ce1b29bf0113978d67d1b9fcff78129e
SirNico <inf19204@lehre.dhbw-stuttgart.de>
2022-07-10 18:54:36 +0200

    combine resolver and rdf interface

--- family-tree-backend/family_tree_backend/rdf_interface.py
+++ family-tree-backend/family_tree_backend/rdf_interface.py
@@ -1,33 +1,31 @@
-from typing import List, Optional
+from typing import List
 
 import rdflib
 
-def get_relation(
-        graph: rdflib.Graph,
-        subject: rdflib.URIRef,
-        object: rdflib.URIRef) -> Optional[rdflib.URIRef]:
-    pass
+from family_tree_backend import vocab
+
+SOURCE_PATH = "family_tree_data"
 
 
-def get_all_relations(
-        graph: rdflib.Graph,
-        subject: rdflib.URIRef,
-        object: rdflib.URIRef) -> List[rdflib.URIRef]:
-    pass
+def get_nodes_of_type(graph, node_type: vocab.Vocab) -> List[rdflib.URIRef]:
+    result = []
+    generator = graph.subjects(
+        predicate=vocab.RDF.type.uri,
+        object=node_type.uri)
+    for node in generator:
+        result.append(node)
+    return result
 
 
-def add_relation(
-        graph: rdflib.Graph,
-        subject: rdflib.URIRef,
-        predicate: rdflib.URIRef,
-        object: rdflib.URIRef):
-    pass
+def load_graph(ft_name: str) -> rdflib.Graph:
+    graph = rdflib.Graph()
+    graph.parse(f"{SOURCE_PATH}/{ft_name}.ttl")
+    return graph
 
 
-def remove_relation(
-        graph: rdflib.Graph,
-        subject: rdflib.URIRef,
-        predicate: rdflib.URIRef,
-        object: rdflib.URIRef):
-    pass
+def store_graph(graph: rdflib.Graph, ft_name: str):
+    path = f"{SOURCE_PATH}/{ft_name}.ttl"
+    with open(path, "w+") as f:
+        f.write("")
 
+    graph.serialize(destination=path, encoding="utf8")
